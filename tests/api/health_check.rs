6f5e4b376a89424b52263baf05821f5a7aefecfb
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-02-03 21:13:12 -0800

    improvement: create integration tests

---
+++ tests/api/health_check.rs
@@ -0,0 +1,13 @@
+// tests/api/health_check.rs
+
+// dependencies
+use actix_web::{test, App};
+use actix_web_template_lib::routes::health_check;
+
+#[actix_web::test]
+async fn test_health_check() {
+  let app = test::init_service(App::new().service(health_check)).await;
+  let req = test::TestRequest::get().uri("/health_check").to_request();
+  let resp = test::call_service(&app, req).await;
+  assert!(resp.status().is_success());
+}
