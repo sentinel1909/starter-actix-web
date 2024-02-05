68a4711da494a012fa1a55b61b97ab08a49cc2df
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-02-04 18:40:21 -0800

    improvement: made startup function more independent of main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,14 @@
 // src/bin/main.rs
 
 // dependencies
+use actix_web_template_lib::configuration::Config;
 use actix_web_template_lib::startup;
+use std::net::TcpListener;
 
 // main function
 #[actix_web::main]
-async fn main() {
-    startup()
-        .await
-        .expect("Unable to start the server on port 8000")
+async fn main() -> std::io::Result<()> {
+    let app_config = Config::default();
+    let address = TcpListener::bind(format!("{}:{}", app_config.address, app_config.port))?;
+    startup(address)?.await
 }
--- src/lib/startup.rs
+++ src/lib/startup.rs
@@ -1,19 +1,19 @@
 // src/lib/startup.rs
 
 // dependencies
-use crate::configuration::Config;
 use crate::routes::health_check;
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 // startup function, configures the app, returns a server with all the configured routes
-pub async fn startup() -> std::io::Result<()> {
-    let app_config = Config::default();
-    HttpServer::new(|| {
+pub fn startup(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .service(health_check)
             .default_service(web::to(HttpResponse::NotFound))
     })
-    .bind((app_config.address, app_config.port))?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
